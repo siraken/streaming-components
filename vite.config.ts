7d7742746fb2caadb5dbe1f9e65cfac7a29d4b11
Kento Shirasawa <shirasawa@novalumo.com>
2025-07-07 21:54:41 +0900

    fix: update Vite configuration to include HTTPS settings and Tailwind CSS plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    https: {
+      key: './localhost-key.pem',
+      cert: './localhost.pem',
+    },
+  },
+  plugins: [react(), tailwindcss()],
 });
