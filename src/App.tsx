ab78b232a4ff2cfb0a8989f3aecd1ec5c8f426e6
Kento Shirasawa <shirasawa@novalumo.com>
2025-07-08 01:02:08 +0900

    refactor: update App component to use arrow function and add action buttons for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
-function App() {
-  return <div></div>;
-}
-
-export default App;
+export const App = () => {
+  const actions: {
+    name: string;
+    url: string;
+  }[] = [
+    {
+      name: 'Clock',
+      url: 'clock',
+    },
+    {
+      name: 'Counter',
+      url: 'counter',
+    },
+  ];
+  return (
+    <div>
+      <div className="tw-grid">
+        <h1>Casting Components</h1>
+        <div>
+          {actions.map((action) => (
+            <button type="button" key={action.name}>
+              {action.name}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import './styles/globals.css';
 import './styles/style.scss';
-import App from './App';
+import { App } from './App';
 import { Clock } from './pages/clock';
 import { SpeechRecognition } from './pages/speech-recognition';
 
